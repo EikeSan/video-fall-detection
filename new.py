0948ba156dd3ad4401d9f431a59d196db1c7c170
Amin Zayeromali <amin.zayeromali@gmail.com>
2023-09-07 14:52:30 +0330

    Customizing Code ...

--- new.py
+++ new.py
@@ -4,7 +4,7 @@ import cv2
 import time
 
 fitToEllipse = False
-cap = cv2.VideoCapture('queda.mp4')
+cap = cv2.VideoCapture('video-fall-detection/queda.mp4')
 time.sleep(2)
 
 fgbg = cv2.createBackgroundSubtractorMOG2()
@@ -12,24 +12,24 @@ j = 0
 
 while(1):
     ret, frame = cap.read()
-    
+
     #Convert each frame to gray scale and subtract the background
     try:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         fgmask = fgbg.apply(gray)
-        
+
         #Find contours
         contours, _ = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         if contours:
-        
+
             # List to hold all areas
             areas = []
 
             for contour in contours:
                 ar = cv2.contourArea(contour)
                 areas.append(ar)
-            
+
             max_area = max(areas, default = 0)
 
             max_area_index = areas.index(max_area)
@@ -37,28 +37,28 @@ while(1):
             cnt = contours[max_area_index]
 
             M = cv2.moments(cnt)
-            
+
             x, y, w, h = cv2.boundingRect(cnt)
 
             cv2.drawContours(fgmask, [cnt], 0, (255,255,255), 3, maxLevel = 0)
-            
+
             if h < w:
                 j += 1
-                
+
             if j > 10:
-                #print("FALL")
-                #cv2.putText(fgmask, 'FALL', (x, y), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255,255,255), 2)
+                print("FALL" + str(j))
+                cv2.putText(fgmask, 'FALL', (x, y), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (255,255,255), 2)
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
 
             if h > w:
-                j = 0 
+                j = 0
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
 
             cv2.imshow('video', frame)
-        
+
             if cv2.waitKey(33) == 27:
-             break
+                break
     except Exception as e:
         break
 cv2.destroyAllWindows()
