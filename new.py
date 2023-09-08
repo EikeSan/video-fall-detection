7f8d2a3b0697522d5e481b5f888a5caaf3e48437
Amin Zayeromali <amin.zayeromali@gmail.com>
2023-09-08 17:31:10 +0330

    Customizing Code ...

--- new.py
+++ new.py
@@ -47,7 +47,7 @@ while(1):
 
             if j > 10:
                 print("FALL" + str(j))
-                cv2.putText(frame, 'Fall Detected', (x-5, y-5), cv2.FONT_HERSHEY_TRIPLEX, 1.0, (255, 255, 255), 3)
+                cv2.putText(frame, 'Fall Detected', (x-7, y-7), cv2.FONT_HERSHEY_TRIPLEX, 1.0, (255, 255, 255), 3)
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
 
             if h > w:
